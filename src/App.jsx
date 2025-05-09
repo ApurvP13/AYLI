940b6f3cd66106353ff589bc47483e1042cb8e5b
Apurv Pandey <apurvpandey1306@gmail.com>
2025-05-09 14:56:47 +0530

    smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 
 import Hero from "./sections/Hero";
@@ -6,8 +6,33 @@ import Header from "./components/Header";
 import Features from "./sections/Features";
 import Footer from "./components/Footer";
 import Extensions from "./components/Extensions";
+import Lenis from "@studio-freight/lenis";
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (x) =>
+        x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2, // smooth easing
+      smooth: true,
+      direction: "vertical", // or 'horizontal'
+      gestureDirection: "vertical",
+      smoothTouch: false,
+      touchMultiplier: 2,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <>
       <Header />
